435a5e564a9fa9f82e16b6973cd27a2c4af4eb95
Pratap Neaupane <sereversion@gmail.com>
2020-05-04 10:21:46 +0545

    Now Dicee can generate new number from 1-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,7 +18,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,20 +35,24 @@ class DicePage extends StatelessWidget {
           children: <Widget>[
             Expanded(
               child: FlatButton(
-                child: Image.asset('images/dice1.png'),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
                 onPressed: () {
-                  print('Left Button Pressed');
+                  setState(() {
+                    leftDiceNumber = 1 + Random().nextInt(6);
+                  });
                 },
               ),
             ),
             Expanded(
               child: FlatButton(
-                child: Image.asset('images/dice6.png'),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
                 onPressed: () {
-                  print('Right Button got Pressed');
+                  setState(() {
+                    rightDiceNumber = 1 + Random().nextInt(6);
+                  });
                 },
               ),
-            )
+            ),
           ],
         ),
       ),
